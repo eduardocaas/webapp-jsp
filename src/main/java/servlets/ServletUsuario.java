a70487f7108c517029bbb19775c900d732daa5e5
Eduardo Castro <eduardofc.dev@gmail.com>
2022-11-01 23:46:28 -0300

    Update ServletUsuario.java

--- src/main/java/servlets/ServletUsuario.java
+++ src/main/java/servlets/ServletUsuario.java
@@ -1,11 +1,15 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.ModelLogin;
 
 /**
  * Servlet implementation class ServletUsuario
@@ -16,8 +20,7 @@ public class ServletUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     public ServletUsuario() {
-        super();
-      
+    	
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,6 +31,20 @@ public class ServletUsuario extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String id = request.getParameter("id");
+		String nome = request.getParameter("nome");
+		String login = request.getParameter("login");
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		
+		ModelLogin modelLogin = new ModelLogin();
+		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null); // recebe uma string, se não for nulo ou vazio, converte pra long, senão, recebe nulo 
+		modelLogin.setNome(nome);
+		modelLogin.setEmail(email);
+		modelLogin.setSenha(senha);
+		
+		RequestDispatcher redirect = request.getRequestDispatcher("principal/usuario.jsp");
+		redirect.forward(request, response);
 		
 	}
 
