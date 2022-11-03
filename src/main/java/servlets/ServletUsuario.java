6e4a818e84cf1713eec9952f81d81fc7737cf01c
Eduardo Castro <eduardofc.dev@gmail.com>
2022-11-02 23:18:55 -0300

    Update ServletUsuario

--- src/main/java/servlets/ServletUsuario.java
+++ src/main/java/servlets/ServletUsuario.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UserDAO;
 import model.ModelLogin;
 
 /**
@@ -18,6 +19,8 @@ import model.ModelLogin;
 public class ServletUsuario extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private UserDAO userDAO = new UserDAO();
        
     public ServletUsuario() {
     	
@@ -31,6 +34,9 @@ public class ServletUsuario extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		try {
+		
+		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String nome = request.getParameter("nome");
 		String login = request.getParameter("login");
@@ -41,10 +47,24 @@ public class ServletUsuario extends HttpServlet {
 		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null); // recebe uma string, se não for nulo ou vazio, converte pra long, senão, recebe nulo 
 		modelLogin.setNome(nome);
 		modelLogin.setEmail(email);
+		modelLogin.setLogin(login);
 		modelLogin.setSenha(senha);
 		
+		modelLogin = userDAO.userRegistration(modelLogin);
+		
+		request.setAttribute("msg", "Operação realizada com sucesso!");
+		request.setAttribute("modelLogin", modelLogin); // manter os dados na tela ao enviar formulario, definido também value padrão no jsp usuario
 		RequestDispatcher redirect = request.getRequestDispatcher("principal/usuario.jsp");
 		redirect.forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirect = request.getRequestDispatcher("/error.jsp");
+			request.setAttribute("msg" , e.getMessage());
+			redirect.forward(request, response);
+		}
+		
+		
 		
 	}
 
