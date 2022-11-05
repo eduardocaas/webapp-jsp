bf9f7f91c89ba504ed290d6034ae2107eba8b123
Eduardo Castro <eduardofc.dev@gmail.com>
2022-11-04 23:31:06 -0300

    Delete function - WIP

--- src/main/java/servlets/ServletUsuario.java
+++ src/main/java/servlets/ServletUsuario.java
@@ -12,9 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import dao.UserDAO;
 import model.ModelLogin;
 
-/**
- * Servlet implementation class ServletUsuario
- */
+
 @WebServlet("/ServletUsuario")
 public class ServletUsuario extends HttpServlet {
 	
@@ -26,19 +24,25 @@ public class ServletUsuario extends HttpServlet {
     	
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // get - deletar e consultar (boa prática)
+		
+		String act = request.getParameter("act"); // input hidden - usuario.jsp
+		
+		if (act != null && !act.isEmpty() && act.equalsIgnoreCase("delete")) {
+			
+			String idUser = request.getParameter("id");
+		}
 		
 		
 	}
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // post - gravar e atualizar (boa prática)
 		
 		try {
 			
 		String msg = "Operação realizada com sucesso!";
 		
-		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String nome = request.getParameter("nome");
 		String login = request.getParameter("login");
@@ -46,17 +50,25 @@ public class ServletUsuario extends HttpServlet {
 		String senha = request.getParameter("senha");
 		
 		ModelLogin modelLogin = new ModelLogin();
+		
 		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null); // recebe uma string, se não for nulo ou vazio, converte pra long, senão, recebe nulo 
 		modelLogin.setNome(nome);
 		modelLogin.setEmail(email);
 		modelLogin.setLogin(login);
 		modelLogin.setSenha(senha);
 		
-		if(userDAO.loginValidate(modelLogin.getLogin()) && modelLogin.getId() == null) { // se já existe o login, e estou tentando gravar um novo usuário (não conflitar com update)
+		if (userDAO.loginValidate(modelLogin.getLogin()) && modelLogin.getId() == null) { // se já existe o login, e estou tentando gravar um novo usuário (não conflitar com update)
 			
 			msg = "Login já existente, informe outro login!";
 			
 		} else {
+			
+			if (modelLogin.isNew()) {
+				msg = "Gravado com sucesso!";
+			} else {
+				msg = "Atualizado com sucesso!";
+			}
+			
 			modelLogin = userDAO.userRegistration(modelLogin);
 		}
 		
@@ -64,8 +76,7 @@ public class ServletUsuario extends HttpServlet {
 		
 		request.setAttribute("msg", msg);
 		request.setAttribute("modelLogin", modelLogin); // manter os dados na tela ao enviar formulario, definido também value padrão no jsp usuario
-		RequestDispatcher redirect = request.getRequestDispatcher("principal/usuario.jsp");
-		redirect.forward(request, response);
+		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
