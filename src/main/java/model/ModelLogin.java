a7544b0c81c36809701d48c40a911c9bdc23ff35
Eduardo Castro <eduardofc.dev@gmail.com>
2022-11-26 00:02:20 -0300

    Added address model

--- src/main/java/model/ModelLogin.java
+++ src/main/java/model/ModelLogin.java
@@ -17,6 +17,13 @@ public class ModelLogin implements Serializable {
 	private String fotouser_extensao;
 	private boolean useradmin;
 	
+	private String cep;
+	private String logradouro;
+	private String bairro;
+	private String localidade;
+	private String uf;
+	private String numero;
+	
 	
 	public boolean isNew() {
 		
@@ -101,6 +108,55 @@ public class ModelLogin implements Serializable {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
 	
 	
 	
